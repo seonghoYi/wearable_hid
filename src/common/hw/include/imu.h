d92bbb191837034d2e4cf46f2f71b290960ef2ba
seonghoYi <sh.Lee4781@gmail.com>
2022-07-07 21:11:20 +0900

    버그 수정 및 코드 개선

--- src/common/hw/include/imu.h
+++ src/common/hw/include/imu.h
@@ -1,29 +1,71 @@
-#ifndef SRC_COMMON_HW_INCLUDE_IMU_H_
-#define SRC_COMMON_HW_INCLUDE_IMU_H_
+#ifndef SRC_COMMON_HW_INCLUDE_IMU_IMU_H_
+#define SRC_COMMON_HW_INCLUDE_IMU_IMU_H_
 
 
 #include "hw_def.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
 #ifdef _USE_HW_IMU
 
+#include "imu/mpu6050.h"
+#include "imu/MadgwickAHRS.h"
 
-bool imuInit(void);
-bool imuBegin(uint32_t update_hz);
-uint32_t imuUpdate();
-float *imuGetRPY(void);
-float *imuGetQuaternion(void);
-int16_t *imuGetAcc(void);
-int16_t *imuGetGyro(void);
+#define FILTER_NUM    3
+
+class cIMU
+{
+  public:
+    cMPU6050 sensor;
+
+
+    bool b_connected;
+
+    float accRes;
+    float gyrRes;
+
+
+    float ax, ay, az;
+    float gx, gy, gz;
+
+
+    int16_t angle[3];
+    float rpy[3];
+    float quat[4];
+
+    int16_t accRaw[3];
+    int16_t accData[3];
+    int16_t gyroRaw[3];
+    int16_t gyroData[3];
+
+  public:
+    cIMU(uint8_t ch);
+
+    bool begin(uint32_t hz);
+    uint32_t update(void);
+
+    void get_acc(float (&acc)[3]);
+    void get_gyro(float (&gyro)[3]);
+    void get_quat(float (&quat)[4]);
+    void get_rpy(float (&rpy)[3]);
+    
+
+  private:
+    Madgwick filter;
+    uint32_t update_hz;
+    uint32_t update_us;
+    uint32_t tTime;
+
+
+    int32_t gyroADC[3][FILTER_NUM];
+    uint32_t prev_process_time;
+    uint32_t cur_process_time;
+    uint32_t process_time;
+
+    void computeIMU(void);
+
+};
 
 #endif
 
-#ifdef __cplusplus
-}
-#endif
 
-#endif /* SRC_COMMON_HW_INCLUDE_IMU_H_ */
+
+#endif /* SRC_COMMON_HW_INCLUDE_IMU_IMU_H_ */
--- src/hw/driver/imu.cpp
+++ src/hw/driver/imu.cpp
@@ -1,172 +1,194 @@
 #include "imu.h"
-#include "imu/imu.h"
-
-#include "cli.h"
-
-static cIMU imu;
-
-#ifdef _USE_HW_IMU
 
 
-static void cliIMU(cli_args_t *args);
 
-bool imuInit(void)
+
+cIMU::cIMU(uint8_t ch) : sensor(cMPU6050(ch)), tTime(0), cur_process_time(0), process_time(0)
 {
-  bool ret = true;
-
-#ifdef _USE_HW_CLI
-  cliAdd("IMU", cliIMU);
-#endif
-
-
-  return ret;
-}
-
-bool imuBegin(uint32_t update_hz)
-{
-  bool ret = imu.begin(update_hz);
-  return ret;
-}
-
-
-uint32_t imuUpdate()
-{
-  uint32_t ret = imu.update();
-
-  return ret;
-}
-
-
-float *imuGetRPY(void)
-{
-  static float *ret;
-  ret = imu.rpy;
-  return ret;
-}
-
-float *imuGetQuaternion(void)
-{
-  static float *ret;
-  ret = imu.quat;
-  return ret;
-}
-
-int16_t *imuGetAcc(void)
-{
-  static int16_t *ret;
-  ret = imu.accData;
-  return ret;
-}
-
-int16_t *imuGetGyro(void)
-{
-  static int16_t *ret;
-  ret = imu.gyroData;
-  return ret;
-}
-
-#endif
-
-#ifdef _USE_HW_CLI
-
-
-void cliIMU(cli_args_t *args)
-{
-  bool ret = true;
-  uint32_t update_hz = 0;
-  static uint32_t update_ms = 0;
-
-  if (args->argc == 2)
+  for (int i = 0; i< 3; i++)
   {
-    if (args->isStr(0, "begin") == true)
-    {
-      update_hz = args->getData(1);
-      ret = imuBegin(update_hz);
-      if (update_hz == 0)
-      {
-        ret = false;
-      }
-      else
-      {
-        update_ms = 1000 / update_hz;
-      }
+    rpy[i] = 0.0f;
+  }
+  b_connected = false;
+}
 
-      if (ret == true)
-      {
-        cliPrintf("success\r\n");
-      }
-      else
-      {
-        cliPrintf("fail\r\n");
-      }
-    }
-    else if (args->isStr(0, "show") == true)
+bool cIMU::begin(uint32_t hz)
+{
+
+  update_hz = hz;
+  update_us = 1000000 / hz;
+
+  accRes = 2.0/32768.0;     //2g
+  gyrRes = 2000.0/32768.0;  // 2000 dps
+
+
+  
+  b_connected = sensor.begin();
+
+
+  if (b_connected == true)
+  {
+    filter.begin(update_hz);
+
+    while(!sensor.gyroGetCaliDone())
     {
-      if (args->isStr(1, "rpy") == true)
-      {
-        while(cliKeepLoop())
-        {
-          imu.update();
-          cliPrintf("\033[2J");
-          cliPrintf("\033[10;5H");
-          cliPrintf("roll: %+3.3f pitch: %+3.3f yaw: %+3.3f\r\n", imu.rpy[ROLL], imu.rpy[PITCH], imu.rpy[YAW]);
-          delay(update_ms);
-        }
-      }
-      else if (args->isStr(1, "quaternion") == true)
-      {
-        while(cliKeepLoop())
-        {
-          imu.update();
-          cliPrintf("\033[2J");
-          cliPrintf("\033[10;5H");
-          cliPrintf("roll: %+3.3f pitch: %+3.3f yaw: %+3.3f\r\n", imu.rpy[ROLL], imu.rpy[PITCH], imu.rpy[YAW]);
-          delay(update_ms);
-        }
-      }
-      else if (args->isStr(1, "accel") == true)
-      {
-        while(cliKeepLoop())
-        {
-          imu.update();
-          cliPrintf("\033[2J");
-          cliPrintf("\033[10;5H");
-          cliPrintf("x: %+d y: %+d z: %+d\r\n", imu.accData[ROLL], imu.accData[PITCH], imu.accData[YAW]);
-          delay(update_ms);
-        }
-      }
-      else if (args->isStr(1, "gyro") == true)
-      {
-        while(cliKeepLoop())
-        {
-          imu.update();
-          cliPrintf("\033[2J");
-          cliPrintf("\033[10;5H");
-          cliPrintf("roll: %+d pitch: %+d yaw: %+d\r\n", imu.gyroData[ROLL], imu.gyroData[PITCH], imu.gyroData[YAW]);
-          delay(update_ms);
-        }
-      }
-    }
-    else
-    {
-      ret = false;
+      update();
     }
   }
-  else
+
+  
+
+  return b_connected;
+}
+
+
+uint32_t cIMU::update()
+{
+  uint16_t ret_time = 0;
+
+	//static uint32_t tTime;
+
+
+	if( (micros()-tTime) >= update_us )
+	{
+		ret_time = micros()-tTime;
+    tTime = micros();
+
+    //printf("%d: %d\n", sensor.dev_ch, ret_time);
+
+		computeIMU();
+    
+		gyroData[0] = sensor.gyroData[0];
+		gyroData[1] = sensor.gyroData[1];
+		gyroData[2] = sensor.gyroData[2];
+
+    gyroRaw[0]  = sensor.gyroRaw[0];
+    gyroRaw[1]  = sensor.gyroRaw[1];
+    gyroRaw[2]  = sensor.gyroRaw[2];
+
+    accData[0]  = sensor.accData[0];
+    accData[1]  = sensor.accData[1];
+    accData[2]  = sensor.accData[2];
+
+    accRaw[0]   = sensor.accRaw[0];
+    accRaw[1]   = sensor.accRaw[1];
+    accRaw[2]   = sensor.accRaw[2];
+	}
+
+	return ret_time;
+}
+
+
+
+
+void cIMU::computeIMU()
+{
+  //static uint32_t prev_process_time = micros();
+  //static uint32_t cur_process_time = 0;
+  //static uint32_t process_time = 0;
+  uint32_t i;
+  //static int32_t gyroADC[3][FILTER_NUM] = {0,};
+  int32_t gyroAdcSum;
+
+  uint32_t axis;
+
+	sensor.accGetData();
+	sensor.gyroGetData();
+
+
+  //printf("%d\n", prev_process_time);
+
+  for (axis = 0; axis < 3; axis++)
   {
-    ret = false;
+    gyroADC[axis][0] = sensor.gyroData[axis];
+
+
+    gyroAdcSum = 0;
+    for (i=0; i<FILTER_NUM; i++)
+    {
+      gyroAdcSum += gyroADC[axis][i];
+    }
+    sensor.gyroData[axis] = gyroAdcSum/FILTER_NUM;
+    for (i=FILTER_NUM-1; i>0; i--)
+    {
+      gyroADC[axis][i] = gyroADC[axis][i-1];
+    }
+
+    if (abs(sensor.gyroData[axis]) <= 3)
+    {
+      sensor.gyroData[axis] = 0;
+    }
+  }
+
+
+  for( i=0; i<3; i++ )
+  {
+    accRaw[i]   = sensor.accRaw[i];
+    accData[i]  = sensor.accData[i];
+    gyroRaw[i]  = sensor.gyroRaw[i];
+    gyroData[i] = sensor.gyroData[i];
+  }
+
+  ax = (float)sensor.accData[0]*accRes;
+  ay = (float)sensor.accData[1]*accRes;
+  az = (float)sensor.accData[2]*accRes;
+
+  gx = (float)sensor.gyroData[0]*gyrRes;
+  gy = (float)sensor.gyroData[1]*gyrRes;
+  gz = (float)sensor.gyroData[2]*gyrRes;
+
+
+  cur_process_time  = micros();
+  process_time      = cur_process_time-prev_process_time;
+  prev_process_time = cur_process_time;
+
+  if (sensor.gyroGetCaliDone() == true && sensor.accGetCaliDone() == true)
+  {
+    filter.invSampleFreq = (float)process_time/1000000.0f;
+    filter.updateIMU(gx, gy, gz, ax, ay, az);
   }
 
 
-  if (ret == false)
-  {
-    cliPrintf("imu begin update_Hz\r\n");
-    cliPrintf("imu show rpy\r\n");
-    cliPrintf("imu show quaternion\r\n");
-    cliPrintf("imu show accel\r\n");
-    cliPrintf("imu show gyro\r\n");
-  }
+  rpy[0] = filter.getRoll();
+  rpy[1] = filter.getPitch();
+  rpy[2] = filter.getYaw()-180.;
+
+  quat[0] = filter.q0;
+  quat[1] = filter.q1;
+  quat[2] = filter.q2;
+  quat[3] = filter.q3;
+
+  angle[0] = (int16_t)(rpy[0] * 10.);
+  angle[1] = (int16_t)(rpy[1] * 10.);
+  angle[2] = (int16_t)(rpy[1] * 1.);
 }
 
 
-#endif
+void cIMU::get_acc(float (&acc)[3])
+{
+  acc[0] = ax;
+  acc[1] = ay;
+  acc[2] = az;
+}
+
+void cIMU::get_gyro(float (&gyro)[3])
+{
+  gyro[0] = gx;
+  gyro[1] = gy;
+  gyro[2] = gz;
+}
+
+void cIMU::get_quat(float (&quat)[4])
+{
+  quat[0] = this->quat[0];
+  quat[1] = this->quat[1];
+  quat[2] = this->quat[2];
+  quat[3] = this->quat[3];
+}
+
+void cIMU::get_rpy(float (&rpy)[3])
+{
+  rpy[0] = this->rpy[0];
+  rpy[1] = this->rpy[1];
+  rpy[2] = this->rpy[2];
+}
