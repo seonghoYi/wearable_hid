f1285a9ffbd24d59273891887c6067d9555f5f7b
seonghoYi <sh.Lee4781@gmail.com>
2022-07-08 22:43:47 +0900

    FreeRTOS 추가

--- src/ap/ap.cpp
+++ src/ap/ap.cpp
@@ -1,16 +1,28 @@
 #include "ap.h"
 #include "imu/imu.h"
 
-
+/*
 cIMU imu1(_DEF_MPU6050_1);
 cIMU imu2(_DEF_MPU6050_2);
 cIMU imu3(_DEF_MPU6050_3);
 cIMU imu4(_DEF_MPU6050_4);
 cIMU imu5(_DEF_MPU6050_5);
+*/
+
+
+void ledThread(void *args)
+{
+	while(1)
+	{
+		ledToggle(_DEF_LED1);
+		vTaskDelay(100);
+	}
+}
+
 
 void apInit()
 {
-	uartOpen(_DEF_UART1, 38400);
+	//uartOpen(_DEF_UART1, 38400);
 	//cliOpen(_DEF_UART1, 38400);
 	//i2cBegin(_DEF_I2C2, 400);
 	//imuBegin(100);
@@ -28,7 +40,7 @@ void apInit()
 	//imu_tbl[_DEF_MPU6050_3].begin(100);
 	//imu_tbl[_DEF_MPU6050_4].begin(100);
 	//imu_tbl[_DEF_MPU6050_5].begin(100);
-
+	/*
 	imu1.begin(200);
 	ledOn(_DEF_LED1);
 	delay(10);
@@ -47,19 +59,25 @@ void apInit()
 	ledOff(_DEF_LED1);
 	imu5.begin(200);
 	ledOn(_DEF_LED1);
+	*/
+
 	//HC06Printf("Calibration Done!!\n");
+
+	xTaskCreate(ledThread, "ledThread", 256, NULL, 1, NULL);
 }
 
 
 void apMain()
 {
+	/*
 	uint32_t pre_time = millis();
 	uint32_t loop_us = micros();
 
 	float quat[4], euler[3];
 	int16_t *acc, *gyro;
 	char buff[100];
-
+	*/
+	vTaskStartScheduler();
 	while(1)	
 	{
 		//imuUpdate(_DEF_MPU6050_1);
@@ -73,84 +91,15 @@ void apMain()
 		//imu_tbl[_DEF_MPU6050_3].update();
 		//imu_tbl[_DEF_MPU6050_4].update();
 		//imu_tbl[_DEF_MPU6050_5].update();
-		//loop_us = micros();
+		/*
 		imu1.update();
 		imu2.update();
 		imu3.update();
 		imu4.update();
 		imu5.update();
-		//printf("%d\n", micros()-loop_us);
-		//loop_us = micros();
-
-		//acc = imuGetAcc();
-		//quat = imuGetQuaternion();
-		//euler = imuGetRPY(); 
-		//gyro = imuGetGyro();
 
 		if(millis() - pre_time >= 50)
 		{
-
-			//sprintf(buff, "%1.3f %1.3f %1.3f %1.3f", quat[0], quat[1], quat[2], quat[3]);
-			//HC06Printf("imu: x:%d, y:%d, z:%d\n", acc[0], acc[1], acc[2]);
-			/*
-			HC06Printf("@");
-			
-			sprintf(buff, "%d %d %d %1.3f %1.3f %1.3f %1.3f", imu1.accRaw[0], imu1.accRaw[1], imu1.accRaw[2], imu1.quat[0], imu1.quat[1], imu1.quat[2], imu1.quat[3]);
-			HC06Printf("%s,", buff);
-			sprintf(buff, "%d %d %d %1.3f %1.3f %1.3f %1.3f", imu2.accRaw[0], imu2.accRaw[1], imu2.accRaw[2], imu2.quat[0], imu2.quat[1], imu2.quat[2], imu2.quat[3]);
-			HC06Printf("%s,", buff);
-			sprintf(buff, "%d %d %d %1.3f %1.3f %1.3f %1.3f", imu3.accRaw[0], imu3.accRaw[1], imu3.accRaw[2], imu3.quat[0], imu3.quat[1], imu3.quat[2], imu3.quat[3]);
-			HC06Printf("%s,", buff);
-			sprintf(buff, "%d %d %d %1.3f %1.3f %1.3f %1.3f", imu4.accRaw[0], imu4.accRaw[1], imu4.accRaw[2], imu4.quat[0], imu4.quat[1], imu4.quat[2], imu4.quat[3]);
-			HC06Printf("%s,", buff);
-			sprintf(buff, "%d %d %d %1.3f %1.3f %1.3f %1.3f", acc[0], acc[1], acc[2], quat[0], quat[1], quat[2], quat[3]);
-			HC06Printf("%s\n", buff);
-			*/
-			/*
-			HC06Printf("%4d %4d %4d %4d %4d %4d %4d", 
-									(int16_t)(imu1.ax*1000), (int16_t)(imu1.ay*1000), (int16_t)(imu1.az*1000), 
-									(int16_t)(imu1.quat[0]*1000), (int16_t)(imu1.quat[1]*1000), (int16_t)(imu1.quat[2]*1000), (int16_t)(imu1.quat[3]*1000));
-			HC06Printf("%4d %4d %4d %4d %4d %4d %4d", 
-									(int16_t)(imu2.ax*1000), (int16_t)(imu2.ay*1000), (int16_t)(imu2.az*1000), 
-									(int16_t)(imu2.quat[0]*1000), (int16_t)(imu2.quat[1]*1000), (int16_t)(imu2.quat[2]*1000), (int16_t)(imu2.quat[3]*1000));
-			HC06Printf("%4d %4d %4d %4d %4d %4d %4d", 
-									(int16_t)(imu3.ax*1000), (int16_t)(imu3.ay*1000), (int16_t)(imu3.az*1000), 
-									(int16_t)(imu3.quat[0]*1000), (int16_t)(imu3.quat[1]*1000), (int16_t)(imu3.quat[2]*1000), (int16_t)(imu3.quat[3]*1000));
-			HC06Printf("%4d %4d %4d %4d %4d %4d %4d", 
-									(int16_t)(imu4.ax*1000), (int16_t)(imu4.ay*1000), (int16_t)(imu4.az*1000), 
-									(int16_t)(imu4.quat[0]*1000), (int16_t)(imu4.quat[1]*1000), (int16_t)(imu4.quat[2]*1000), (int16_t)(imu4.quat[3]*1000));
-			HC06Printf("%4d %4d %4d %4d %4d %4d %4d\n", 
-									(int16_t)(acc[0]*1000), (int16_t)(acc[1]*1000), (int16_t)(acc[2]*1000), 
-									(int16_t)(quat[0]*1000), (int16_t)(quat[1]*1000), (int16_t)(quat[2]*1000), (int16_t)(quat[3]*1000));
-			*/
-			/*
-			HC06Printf("@");
-			sprintf(buff, "%d %d %d %d %d %d %d,", 
-									(int16_t)(imu1.ax*1000), (int16_t)(imu1.ay*1000), (int16_t)(imu1.az*1000), 
-									(int16_t)(imu1.quat[0]*1000), (int16_t)(imu1.quat[1]*1000), (int16_t)(imu1.quat[2]*1000), (int16_t)(imu1.quat[3]*1000));
-			HC06Write((uint8_t *)buff, strlen(buff));
-
-			sprintf(buff, "%d %d %d %d %d %d %d,", 
-									(int16_t)(imu2.ax*1000), (int16_t)(imu2.ay*1000), (int16_t)(imu2.az*1000), 
-									(int16_t)(imu2.quat[0]*1000), (int16_t)(imu2.quat[1]*1000), (int16_t)(imu2.quat[2]*1000), (int16_t)(imu2.quat[3]*1000));
-			HC06Write((uint8_t *)buff, strlen(buff));
-			
-			sprintf(buff, "%d %d %d %d %d %d %d,", 
-									(int16_t)(imu3.ax*1000), (int16_t)(imu3.ay*1000), (int16_t)(imu3.az*1000), 
-									(int16_t)(imu3.quat[0]*1000), (int16_t)(imu3.quat[1]*1000), (int16_t)(imu3.quat[2]*1000), (int16_t)(imu3.quat[3]*1000));
-			HC06Write((uint8_t *)buff, strlen(buff));
-
-			sprintf(buff, "%d %d %d %d %d %d %d,", 
-									(int16_t)(imu4.ax*1000), (int16_t)(imu4.ay*1000), (int16_t)(imu4.az*1000), 
-									(int16_t)(imu4.quat[0]*1000), (int16_t)(imu4.quat[1]*1000), (int16_t)(imu4.quat[2]*1000), (int16_t)(imu4.quat[3]*1000));
-			HC06Write((uint8_t *)buff, strlen(buff));
-
-			sprintf(buff, "%d %d %d %d %d %d %d\n", 
-									(int16_t)(acc[0]*1000), (int16_t)(acc[1]*1000), (int16_t)(acc[2]*1000), 
-									(int16_t)(quat[0]*1000), (int16_t)(quat[1]*1000), (int16_t)(quat[2]*1000), (int16_t)(quat[3]*1000));
-			HC06Write((uint8_t *)buff, strlen(buff));
-			*/
-			
 			printf("@");
 			printf("%d %d %d %d %d %d %d,", 
 									(int16_t)(imu2.ax*1000), (int16_t)(imu2.ay*1000), (int16_t)(imu2.az*1000), 
@@ -180,6 +129,7 @@ void apMain()
 			//HC06Printf("%d\n", millis());
 			pre_time = millis();
 		}
+		*/
 		//printf("1: %3.3f %3.3f %3.3f\n", imu1.rpy[0], imu1.rpy[1], imu1.rpy[2]);
 		//imuGetRPY(_DEF_MPU6050_1, euler);
 		//imu_tbl[_DEF_MPU6050_1].get_rpy(euler);
