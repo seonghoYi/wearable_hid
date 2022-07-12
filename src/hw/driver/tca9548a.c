226677d7a003384798dc8631d097662c36491b5e
seonghoYi <sh.Lee4781@gmail.com>
2022-07-13 00:03:34 +0900

    rtos 대응 mutex 추가

--- src/hw/driver/tca9548a.c
+++ src/hw/driver/tca9548a.c
@@ -5,6 +5,11 @@
 #ifdef _USE_HW_TCA9548A
 
 
+#ifdef _USE_HW_RTOS
+SemaphoreHandle_t mutex = NULL;
+#endif
+
+
 #define TCA9548A_I2C_ADDRESS  0x70 << 1
 
 typedef struct
@@ -33,6 +38,14 @@ bool tca9548Init(uint32_t freq_khz)
 
   ret &= i2cBegin(tca9548_handle.i2c_ch, freq_khz);
 
+#ifdef _USE_HW_RTOS
+  mutex = xSemaphoreCreateMutex();
+  if (mutex == NULL)
+  {
+    printf("mutex fail\n");
+  }
+#endif
+
   if(ret)
   {
     tca9548_handle.is_init = true;
@@ -45,6 +58,11 @@ bool tca9548IsDeviceReady(uint8_t ch, uint8_t dev_addr)
 {
   bool ret = false;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -55,6 +73,9 @@ bool tca9548IsDeviceReady(uint8_t ch, uint8_t dev_addr)
   }
   
   ret = i2cIsDeviceReady(tca9548_handle.i2c_ch, dev_addr);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -62,6 +83,11 @@ uint32_t tca9548Write(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t s
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -72,6 +98,9 @@ uint32_t tca9548Write(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t s
   }
 
   ret = i2cWrite(tca9548_handle.i2c_ch, dev_addr, p_data, size);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -79,6 +108,11 @@ uint32_t tca9548Read(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t si
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -89,6 +123,9 @@ uint32_t tca9548Read(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t si
   }
 
   ret = i2cRead(tca9548_handle.i2c_ch, dev_addr, p_data, size);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -96,6 +133,11 @@ uint32_t tca9548MemWrite(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint1
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -106,6 +148,9 @@ uint32_t tca9548MemWrite(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint1
   }
 
   ret = i2cMemWrite(tca9548_handle.i2c_ch, dev_addr, mem_addr, mem_addr_size, data);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -113,6 +158,11 @@ uint32_t tca9548MemWrites(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -123,6 +173,9 @@ uint32_t tca9548MemWrites(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint
   } 
 
   ret = i2cMemWrites(tca9548_handle.i2c_ch, dev_addr, mem_addr, mem_addr_size, p_data, size);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -130,6 +183,11 @@ uint32_t tca9548MemRead(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint16
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -140,6 +198,9 @@ uint32_t tca9548MemRead(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint16
   }
 
   ret = i2cMemRead(tca9548_handle.i2c_ch, dev_addr, mem_addr, mem_addr_size, p_data);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
@@ -147,6 +208,11 @@ uint32_t tca9548MemReads(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint1
 {
   uint32_t ret = 0;
   uint8_t mux_ch = 1 << ch;
+
+#ifdef _USE_HW_RTOS
+  while (xSemaphoreTake(mutex, 10) != pdTRUE);
+#endif
+
   if (tca9548_handle.mux_ch != mux_ch)
   {
     if (i2cWrite(tca9548_handle.i2c_ch, TCA9548A_I2C_ADDRESS, &mux_ch, 1) == 0)
@@ -157,6 +223,9 @@ uint32_t tca9548MemReads(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint1
   } 
 
   ret = i2cMemReads(tca9548_handle.i2c_ch, dev_addr, mem_addr, mem_addr_size, p_data, size);
+#ifdef _USE_HW_RTOS
+  xSemaphoreGive(mutex);
+#endif
   return ret;
 }
 
