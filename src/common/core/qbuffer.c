089d11d9720956004b042e040fe941244d5fb830
seonghoYi <sh.Lee4781@gmail.com>
2022-07-31 15:36:33 +0900

    큐버퍼 개선

--- src/common/core/qbuffer.c
+++ src/common/core/qbuffer.c
@@ -15,6 +15,20 @@ bool qbufferCreate(qbuffer_t *p_node, uint8_t *p_buf, uint32_t length)
 	p_node->out   = 0;
 	p_node->len   = length;
 	p_node->p_buf = p_buf;
+	p_node->size  = 1;
+
+	return ret;
+}
+
+bool qbufferCreateBySize(qbuffer_t *p_node, uint8_t *p_buf, uint32_t size, uint32_t length)
+{
+	bool ret = true;
+
+	p_node->in    = 0;
+	p_node->out   = 0;
+	p_node->len   = length;
+	p_node->p_buf = p_buf;
+	p_node->size  = size;
 
 	return ret;
 }
@@ -23,6 +37,12 @@ bool qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 {
 	bool ret = 0;
 	uint32_t next_in;
+	uint32_t size = p_node->size;
+
+	if (p_data == NULL)
+	{
+		return ret;
+	}
 
 	for (int i=0; i<length; i++)
 	{
@@ -32,7 +52,11 @@ bool qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 		{
 			if (p_node->p_buf != NULL)
 			{
-				p_node->p_buf[p_node->in] = p_data[i];
+				for (int j=0; j<size; j++)
+				{
+					p_node->p_buf[p_node->in*size+j] = p_data[i*size+j];
+				}
+				
 			}
 			p_node->in = next_in;
 		}
@@ -49,12 +73,21 @@ bool qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 bool qbufferRead(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 {
 	bool ret = true;
+	uint32_t size = p_node->size;
+
+	if (p_data == NULL)
+	{
+		return false;
+	}
 
 	for (int i=0; i<length; i++)
 	{
 		if (p_node->p_buf != NULL)
 		{
-			p_data[i] = p_node->p_buf[p_node->out];
+			for (int j=0; j<size; j++)
+			{
+				p_data[i*size] = p_node->p_buf[p_node->out];
+			}
 		}
 
 		if (p_node->out != p_node->in)
--- src/common/core/qbuffer.h
+++ src/common/core/qbuffer.h
@@ -9,6 +9,7 @@ typedef struct
 	uint32_t in;
 	uint32_t out;
 	uint32_t len;
+	uint32_t size;
 
 	uint8_t *p_buf;
 } qbuffer_t;
@@ -17,6 +18,7 @@ typedef struct
 
 void      qbufferInit(void);
 bool      qbufferCreate(qbuffer_t *p_node, uint8_t *p_buf, uint32_t length);
+bool      qbufferCreateBySize(qbuffer_t *p_node, uint8_t *p_buf, uint32_t size, uint32_t length);
 bool      qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length);
 bool      qbufferRead(qbuffer_t *p_node, uint8_t *p_data, uint32_t length);
 uint32_t  qbufferAvailable(qbuffer_t *p_node);
