9d226e732d797e82a818b021e3656d9d8a5c60d5
seonghoYi <sh.Lee4781@gmail.com>
2022-08-17 16:24:51 +0900

    틱 변경

--- src/bsp/FreeRTOSConfig.h
+++ src/bsp/FreeRTOSConfig.h
@@ -11,7 +11,7 @@
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      133000000
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 100 )
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES                    32
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 128
 #define configMAX_TASK_NAME_LEN                 20
